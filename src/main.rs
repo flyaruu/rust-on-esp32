0645f38093af1f4e718fff3780c47c4748eb3081
Frank Lyaruu <f.lyaruu@quin.nl>
2023-07-23 17:25:16 +0200

    part 3

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
+use std::{thread::sleep, time::Duration, sync::{Mutex, Arc}};
 
 use embedded_svc::wifi::{Configuration, ClientConfiguration, AuthMethod};
-use esp_idf_hal::{peripheral::Peripheral, prelude::Peripherals};
+use esp_idf_hal::{peripheral::Peripheral, prelude::Peripherals, gpio::PinDriver};
 use esp_idf_svc::{eventloop::EspSystemEventLoop, nvs::{EspNvsPartition, NvsDefault, EspDefaultNvsPartition}, timer::{EspTimerService, Task, EspTaskTimerService}, wifi::{AsyncWifi, EspWifi}, ping::EspPing, http::server::EspHttpServer};
 use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
 use log::*;
@@ -25,56 +26,63 @@ fn main() {
 
     let mut server = EspHttpServer::new(&Default::default()).unwrap();
 
+    let led_pin = Arc::new(Mutex::new(PinDriver::output(peripherals.pins.gpio5).unwrap()));
+
     server.fn_handler("/", embedded_svc::http::Method::Get, move |req| {
         let mut response = req.into_ok_response().unwrap();
         response.write("Hello from Esp32-c3".as_bytes()).unwrap();
+        led_pin.lock().unwrap().toggle().unwrap();
         Ok(())
     }).unwrap();
+
+    loop {
+        sleep(Duration::from_secs(1));
+    }
 }
 
-    pub fn wifi(
-        modem: impl Peripheral<P = esp_idf_hal::modem::Modem> + 'static,
-        sysloop: EspSystemEventLoop,
-        nvs: Option<EspNvsPartition<NvsDefault>>,
-        timer_service: EspTimerService<Task>,
-    ) -> anyhow::Result<AsyncWifi<EspWifi<'static>>> {
-        use futures::executor::block_on;
-        let mut wifi = AsyncWifi::wrap(
-            EspWifi::new(modem, sysloop.clone(), nvs)?,
-            sysloop,
-            timer_service.clone(),
-        )?;
-    
-        block_on(connect_wifi(&mut wifi))?;
-    
-        let ip_info = wifi.wifi().sta_netif().get_ip_info()?;
-    
-        println!("Wifi DHCP info: {:?}", ip_info);
-        
-        EspPing::default().ping(ip_info.subnet.gateway, &embedded_svc::ping::Configuration::default())?;
-        Ok(wifi)
-    
-    }
+pub fn wifi(
+    modem: impl Peripheral<P = esp_idf_hal::modem::Modem> + 'static,
+    sysloop: EspSystemEventLoop,
+    nvs: Option<EspNvsPartition<NvsDefault>>,
+    timer_service: EspTimerService<Task>,
+) -> anyhow::Result<AsyncWifi<EspWifi<'static>>> {
+    use futures::executor::block_on;
+    let mut wifi = AsyncWifi::wrap(
+        EspWifi::new(modem, sysloop.clone(), nvs)?,
+        sysloop,
+        timer_service.clone(),
+    )?;
 
-    async fn connect_wifi(wifi: &mut AsyncWifi<EspWifi<'static>>) -> anyhow::Result<()> {
-        let wifi_configuration: Configuration = Configuration::Client(ClientConfiguration {
-            ssid: SSID.into(),
-            bssid: None,
-            auth_method: AuthMethod::WPA2Personal,
-            password: PASS.into(),
-            channel: None,
-        });
+    block_on(connect_wifi(&mut wifi))?;
+
+    let ip_info = wifi.wifi().sta_netif().get_ip_info()?;
+
+    println!("Wifi DHCP info: {:?}", ip_info);
     
-        wifi.set_configuration(&wifi_configuration)?;
-    
-        wifi.start().await?;
-        info!("Wifi started");
-    
-        wifi.connect().await?;
-        info!("Wifi connected");
-    
-        wifi.wait_netif_up().await?;
-        info!("Wifi netif up");
-    
-        Ok(())
-    }
+    EspPing::default().ping(ip_info.subnet.gateway, &embedded_svc::ping::Configuration::default())?;
+    Ok(wifi)
+
+}
+
+async fn connect_wifi(wifi: &mut AsyncWifi<EspWifi<'static>>) -> anyhow::Result<()> {
+    let wifi_configuration: Configuration = Configuration::Client(ClientConfiguration {
+        ssid: SSID.into(),
+        bssid: None,
+        auth_method: AuthMethod::WPA2Personal,
+        password: PASS.into(),
+        channel: None,
+    });
+
+    wifi.set_configuration(&wifi_configuration)?;
+
+    wifi.start().await?;
+    info!("Wifi started");
+
+    wifi.connect().await?;
+    info!("Wifi connected");
+
+    wifi.wait_netif_up().await?;
+    info!("Wifi netif up");
+
+    Ok(())
+}
